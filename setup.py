10bb2747617ad9bb97109fe69b1cfa678340aa2c
Guillaume David <guillaumedavidphd@users.noreply.github.com>
2023-05-12 18:19:38 -0700

    declare `mplstyle` file in `MANIFEST.in`

--- setup.py
+++ setup.py
@@ -2,24 +2,26 @@ import atexit
 from setuptools import setup
 from setuptools.command.install import install
 
+
 def _post_install():
     import oldplotlib
+
     oldplotlib.copy_style()
+
 
 class new_install(install):
     def __init__(self, *args, **kwargs):
         super(new_install, self).__init__(*args, **kwargs)
         atexit.register(_post_install)
 
-__version__ = '0.1.0'
+
+__version__ = "0.1.0"
 
 setup(
-    name              = 'oldplotlib',
-    version           = __version__,
-    install_requires  = ['matplotlib>=3.0.0'],
-    packages          = ['oldplotlib'],
-    cmdclass          = {'install': new_install},
-    package_data      = {'oldplotlib/styles':[
-        'oldplotlib/styles/oldplotlib.mplstyle',
-    ]},
+    name="oldplotlib",
+    version=__version__,
+    install_requires=["matplotlib>=3.0.0"],
+    packages=["oldplotlib"],
+    cmdclass={"install": new_install},
+    include_package_data=True,
 )
